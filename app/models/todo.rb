7559aba5a86cf36c40353b683026f45125251f3b
James Young <jpblicer@gmail.com>
2024-08-20 15:11:59 +0900

    update Todo status to default to not_started

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ApplicationRecord
   belongs_to :todoable, polymorphic: true, optional: true
   belongs_to :user
-  enum status: { not_started: 0, in_progress: 1, done: 2 }
+  enum status: { not_started: 0, in_progress: 1, done: 2 }, default: 0
 end
