0e3bdba61cdf6237f6d60a182e94b50a8be6d369
James Young <jpblicer@gmail.com>
2024-08-26 11:01:00 +0900

    setup company create and new with simple_form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,26 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
     @page_title = 'Company Details'
   end
+
+  def new
+    @company = Company.new
+    @page_title = 'New Company'
+  end
+
+  def create
+    @company = Company.new(company_params)
+    @company.user = current_user
+
+    if @company.save
+      redirect_to @company
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:title, :body)
+  end
 end
