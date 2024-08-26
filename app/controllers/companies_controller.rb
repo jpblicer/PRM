a564d55390c867610cf724520d6a517309562370
James Young <jpblicer@gmail.com>
2024-08-26 10:41:10 +0900

    add company index and layout and style bootstrap

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,9 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+    @page_title = "Companies"
+  end
+
   def show
     @company = Company.find(params[:id])
     @page_title = 'Company Details'
