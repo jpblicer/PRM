249df21237b319330aaa5eacf7b8828bb1def61a
James Young <jpblicer@gmail.com>
2024-08-26 16:18:59 +0900

    create a new instance of company when it is not found in the list of existing companies for contact

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,17 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 import TomSelect from "tom-select";
+import { post } from '@rails/request.js'
 
 // Connects to data-controller="tom-select"
 export default class extends Controller {
   static values = { options: Object }
+  static targets = ['item']
 
   connect() {
     new TomSelect(
       this.element,
       {
+        create: (input, callback) => {
+          if(prompt('New Company', input)) {
+            post(`/companies?name=${input}`, { responseKind: 'turbo-stream' })
+          } else {
+            callback(false)
+          }
+        },
         optionClass: 'option',
 	      itemClass: 'item',
       }
     );
+  }
+  itemTargetConnected(element) {
+    console.log("Item target called")
+    this.element.tomselect.sync()
+    this.element.tomselect.setValue(element.value)
   }
 }
