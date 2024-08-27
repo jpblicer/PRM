ba4692f1012ee71078f180061504d0907ec9fc53
James Young <jpblicer@gmail.com>
2024-08-27 14:03:57 +0900

    filters added and broken dashboard into partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,15 @@ class PagesController < ApplicationController
     events = Event.all
     combined_records = todos.to_a + events.to_a
     @sorted_records = combined_records.sort_by { |record| record.end_date || Time.at(0) }
+
+    @sorted_records = case params[:filter]
+                      when 'events'
+                        @sorted_records.select { |record| record.is_a?(Event) }
+                      when 'todos'
+                        @sorted_records.select { |record| record.is_a?(Todo) }
+                      else
+                        @sorted_records
+                      end
     @page_title = 'Dashboard'
   end
 end
