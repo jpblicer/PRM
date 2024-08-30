5ea1651b2fdc7d4c8dd3949cd9868aaddd90d4cb
James Y. <94524979+jpblicer@users.noreply.github.com>
2024-08-30 15:33:21 +0900

    Revert "Revert "todos are archiveable now""

--- app/javascript/controllers/todo_controller.js
+++ app/javascript/controllers/todo_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="todo"
 export default class extends Controller {
   submit() {
-    this.element.submit()
+    // this.element.submit()
+    Turbo.navigator.submitForm(this.element)
   }
 }
